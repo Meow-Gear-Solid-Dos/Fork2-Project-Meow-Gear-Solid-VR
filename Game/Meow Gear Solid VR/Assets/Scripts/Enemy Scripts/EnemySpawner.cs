6b1cb731839afd4187ea063b03b69649b924d67e
ethnpan07 <ethanpan@cpp.edu>
2024-03-13 16:34:42 -0700

    add enemy spawner, not fully working yet

---
+++ Game/Meow Gear Solid VR/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private float _spawnRadius;
+
+    [SerializeField]
+    private bool _isOn;
+
+    [SerializeField]
+    private int _maxSpawnNum;
+
+    private bool canSpawn = false;
+    private bool alreadySpawned = false;
+
+    private void Start()
+    {
+        if (_isOn) { 
+            StartCoroutine(Spawner());
+        }
+    }
+
+    void Update()
+    {
+        if(Mathf.Abs (Vector3.Distance(_player.transform.position, transform.position)) < _spawnRadius && !alreadySpawned)
+        { 
+            canSpawn= true;
+        }
+    }
+
+    private IEnumerator Spawner()
+    {
+        Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(-4.0f,4.0f), transform.position.y, transform.position.z);
+        int counter = 0;
+        while (counter < _maxSpawnNum && canSpawn)
+        { 
+            Instantiate(_enemyPrefab, spawnPoint, Quaternion.identity);
+            counter++;
+            spawnPoint.y = spawnPoint.y + Random.Range(-4.0f, 4.0f);
+        }
+        alreadySpawned = true;
+        yield return null;
+    }
+
+    
+}
