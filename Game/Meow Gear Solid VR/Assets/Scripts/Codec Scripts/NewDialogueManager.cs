d7c4ce6ba9cabc94b0e6083f82f7ac31935d25c8
Xylathoth <bill9192003@gmail.com>
2024-03-15 11:31:21 -0700

    Dialogue scripts mostly implemented
    Working on simple ui and buttons next to display display dialogue before fully implementing dialogue system

--- Game/Meow Gear Solid VR/Assets/Scripts/Codec Scripts/NewDialogueManager.cs
+++ Game/Meow Gear Solid VR/Assets/Scripts/Codec Scripts/NewDialogueManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewDialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
     // Puts all sentences we are going to display into queue
     public Queue<string> sentences;
 
@@ -22,6 +26,38 @@ public class NewDialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
+        // Starts dialogue
         Debug.Log("Answering Codec: " + dialogue.callerName);
+
+        nameText.text = dialogue.callerName;
+
+        // Clears any previous sentences
+        sentences.Clear();
+
+        // Loop through sentences and add to queue
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        // If there is no sentences in queue, end dialogue
+        if (sentences.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
     }
 }
