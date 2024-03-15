9c9024e403e7df5d979fcf2cb6cc73f74f3f1e34
ethnpan07 <ethanpan@cpp.edu>
2024-03-15 14:00:13 -0700

    add enemy spawner

--- Game/Meow Gear Solid VR/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
+++ Game/Meow Gear Solid VR/Assets/Scripts/Enemy Scripts/EnemySpawner.cs
@@ -23,32 +23,38 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        if (_isOn) { 
+        if (_isOn)
+        {
             StartCoroutine(Spawner());
         }
     }
 
     void Update()
     {
-        if(Mathf.Abs (Vector3.Distance(_player.transform.position, transform.position)) < _spawnRadius && !alreadySpawned)
-        { 
-            canSpawn= true;
+        if (Mathf.Abs(Vector3.Distance(_player.transform.position, transform.position)) < _spawnRadius && !alreadySpawned)
+        {
+            canSpawn = true;
         }
     }
 
     private IEnumerator Spawner()
     {
-        Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(-4.0f,4.0f), transform.position.y, transform.position.z);
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(-4.0f, 4.0f), transform.position.y, transform.position.z);
         int counter = 0;
-        while (counter < _maxSpawnNum && canSpawn)
-        { 
-            Instantiate(_enemyPrefab, spawnPoint, Quaternion.identity);
-            counter++;
-            spawnPoint.y = spawnPoint.y + Random.Range(-4.0f, 4.0f);
+        while (true && counter < _maxSpawnNum)
+        {
+            yield return wait;
+            if (canSpawn && counter < _maxSpawnNum)
+            {
+                Instantiate(_enemyPrefab, spawnPoint, Quaternion.identity);
+                counter++;
+                spawnPoint.y = spawnPoint.y + Random.Range(-4.0f, 4.0f);
+            }
+
         }
-        alreadySpawned = true;
-        yield return null;
+
     }
 
-    
+
 }
