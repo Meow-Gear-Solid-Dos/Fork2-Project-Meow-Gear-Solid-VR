69fe3aca71e33b2c3daab1d48556565d82a1e5c6
AlecUrbany <alecurbany@gmail.com>
2024-03-20 13:44:13 -0700

    Implemented Aidan's scripts and scene

---
+++ Game/Meow Gear Solid VR/Assets/Scripts/Player Scripts/Entity_Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity_Player : MonoBehaviour{
+    public Camera CameraReference;
+    public Entity_Statistics EntityStatistics;
+    public Transform Transform;
+    public GameObject Cat;
+    // Start is called before the first frame update
+
+    void Awake(){
+        EntityStatistics = new Entity_Statistics();
+
+        CameraReference = Camera.main;
+
+        EntityStatistics.Jumps = 1;
+        EntityStatistics.MovementSpeed = 5.0f;
+
+        Transform = Cat.GetComponent<Transform>();
+    }
+
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Quaternion Rotation = Quaternion.Euler(0.0f, CameraReference.transform.localEulerAngles.y, 0.0f);
+
+        Transform.rotation = Rotation;
+    }
+}
