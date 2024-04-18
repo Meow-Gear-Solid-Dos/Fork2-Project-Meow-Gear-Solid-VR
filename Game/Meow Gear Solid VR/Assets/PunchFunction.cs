07a38f1c0246da69b4dc05c659634b95d697a3fb
AlecUrbany <alecurbany@gmail.com>
2024-04-18 00:51:47 -0700

    Punching function has finally been implemented!

--- Game/Meow Gear Solid VR/Assets/PunchFunction.cs
+++ Game/Meow Gear Solid VR/Assets/PunchFunction.cs
@@ -7,30 +7,61 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class PunchFunction : MonoBehaviour
 {
     [SerializeField] private GameObject HitBoxPrefab;
-    [SerializeField] private Transform rightFist;
-    [SerializeField] private Transform leftFist;
-    public Rigidbody LeftControllerDevice;
-    public Rigidbody RightControllerDevice;
-    public Vector3 LeftControllerVelocity;
-    public Vector3 RightControllerVelocity;
+    [SerializeField] private Transform fist;
+    public float speed;
     public bool leftGrip;
     public bool rightGrip;
+    public bool punching;
     public float punchVelocityRequirement = 2f;
+
+
+    //Below is for audio function
+    public AudioSource source;
+    public AudioClip whiff1;
+    public AudioClip whiff2;
     // Start is called before the first frame update
     void Start()
     {
+        punching = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((LeftControllerVelocity.magnitude >= punchVelocityRequirement) && leftGrip == true)
+        StartCoroutine(FistSpeed(fist));
+    }
+
+    public IEnumerator FistSpeed(Transform fist)
+    {
+        var previous = fist.localPosition;
+        previous.y = 0;
+        yield return new WaitForSeconds(.001f);
+        var current = fist.localPosition;
+        current.y = 0;
+        speed = ((current - previous).magnitude) / Time.deltaTime;
+        if((speed >= punchVelocityRequirement) && punching == false)
         {
-            GameObject hitBox = Instantiate(HitBoxPrefab, leftFist.position, leftFist.rotation);
+            if(Random.Range(0, 2) == 0 )
+            {
+                punching = true;
+                source.PlayOneShot(whiff2, .25f);
+                StartCoroutine(Timeout(fist));
+
+            }
+
+            else
+            {
+                punching = true;
+                source.PlayOneShot(whiff1, .25f);
+                StartCoroutine(Timeout(fist));	
+            }
         }
-        if((RightControllerVelocity.magnitude >= punchVelocityRequirement) && rightGrip == true)
-        {
-            GameObject hitBox = Instantiate(HitBoxPrefab, rightFist.position, rightFist.rotation);
-        }
+    }
+    IEnumerator Timeout(Transform fist)
+    {
+        GameObject hitBox = Instantiate(HitBoxPrefab, fist.position, fist.rotation);
+		yield return new WaitForSeconds(.3f);	
+        punching = false;
+        
     }
 }
